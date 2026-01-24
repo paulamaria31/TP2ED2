319f99f7ad352b90f6917072b6550bfb1e1250bd
paulamaria31 <mariapaulamirandagasparini@gmail.com>
2026-01-23 21:06:52 -0300

    Atualizando

--- ordenaF1.h
+++ ordenaF1.h
@@ -1,8 +1,19 @@
 #ifndef ordenaF1_h
 #define ordenaF1_h
+#define TAM_VET 19
+#include "Aluno.h"
+#include <stdbool.h>
 #include <stdio.h>
 
-void heap();
+typedef struct Registro
+{
+    Aluno aluno;
+    int marcado;
+}Registro;
+
+void heap(Registro alunos[], int n);
+void refazerHeap (Registro alunos[], int i, int n);
+bool ehMaior(Registro a, Registro b);
 void lerRegistro(int quantidade, FILE* arq);
 
 #endif
