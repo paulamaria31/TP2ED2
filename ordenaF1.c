de131e7b9ece562ac9624ba833f5afaf56231548
paulamaria31 <mariapaulamirandagasparini@gmail.com>
2026-01-26 17:17:34 -0300

    Atualizando

--- ordenaF1.c
+++ ordenaF1.c
@@ -2,48 +2,39 @@
 
 // Meu quantidade igual a 100,200,etc
 void lerRegistro(int quantidade, FILE *arq)
-{   
-    //Numero de fitas
+{
+    // Numero de fitas
     int numFitas = 1;
     char nomeArquivo[20];
     sprintf(nomeArquivo, "Fita%d.bin", numFitas);
-        //Criacao do arquivo bin
-
     FILE *fitaAtual = fopen(nomeArquivo, "wb");
 
-    //Meu vetor de 19 alunos
     Registro alunos[TAM_VET];
     Registro aluno;
     Registro ultimoSair;
+    int contMarcados = 0; // Variável para controlar os marcados de forma eficiente
 
     // Leio os 19 primeiros alunos
-    fread(alunos, sizeof(Registro), TAM_VET, arq);
-    // Construo o heap
-    heap(alunos, TAM_VET);
+    int lidosInicial = fread(alunos, sizeof(Registro), TAM_VET, arq);
+    heap(alunos, lidosInicial);
 
-    int c = TAM_VET;
-    // Contador de registros da fita
-    int cont = 0;
+    int c = lidosInicial;
 
-    //Começo o processo
     while (c < quantidade)
     {
-        // Escrevo o aluno do heap na primeira fita de entrada
+        // Escrevo o aluno do heap na fita de entrada
         ultimoSair = alunos[0];
         fwrite(&ultimoSair, sizeof(Registro), 1, fitaAtual);
 
         // Leio outro aluno para entrar
         if (fread(&aluno, sizeof(Registro), 1, arq) == 1)
         {
-            // Contador da quantidade total
             c++;
             // Vendo se o que entrou agora é menor que o que saiu
             if (aluno.aluno.nota < ultimoSair.aluno.nota)
             {
-                // Marco ele pois é menor, logo vai ter prioridade
                 aluno.marcado = 1;
-                // Atualizo contador da fita
-                cont++;
+                contMarcados++;
             }
             else
             {
@@ -54,34 +45,50 @@ void lerRegistro(int quantidade, FILE *arq)
         }
         else
         {
+            // Se o arquivo original acabar, temos que tratar o fim do arquivo
             break;
         }
 
-        //Refaço o heap com o novo aluno
+        // Refaço o heap com o novo aluno
         refazerHeap(alunos, 0, TAM_VET);
 
-        //rEFAZER PARA VER QUANTOS BLOCOS VAO FICAR EM CADA FITA
-        if (cont == TAM_VET)
+        // Se todos estiverem marcados, está na hora de criar uma nova fita (ou bloco)
+        if (contMarcados == TAM_VET)
         {
             fclose(fitaAtual);
             numFitas++;
-            sprintf(nomeArquivo, "Fita%d.bin", numFitas);
-            fitaAtual = fopen(nomeArquivo, "wb");
+
+            // Lógica circular para 19 fitas
+            int indiceFita = ((numFitas - 1) % 19) + 1;
+            sprintf(nomeArquivo, "Fita%d.bin", indiceFita);
+
+            // Abre nova fita ou anexa se já existir
+            if (numFitas <= 19)
+                fitaAtual = fopen(nomeArquivo, "wb");
+            else
+                fitaAtual = fopen(nomeArquivo, "ab");
 
             for (int i = 0; i < TAM_VET; i++)
             {
                 alunos[i].marcado = 0;
             }
             heap(alunos, TAM_VET);
-            cont = 0;
+            contMarcados = 0;
         }
     }
 
-    //Inicializo o marcado diferentes para os novos registros
+    //Preciso descarregar os 19 que estao na memoria RAM
     for (int i = 0; i < TAM_VET; i++)
     {
-        fwrite(&alunos[0], sizeof(Registro), 1, fitaAtual);
+        //Coloco na fita o aluno com heap
+        ultimoSair = alunos[0];
+        fwrite(&ultimoSair, sizeof(Registro), 1, fitaAtual);
+
+        // 2. Para o próximo menor subir, "matamos" o atual
+        // dando a ele uma marcação impossível (ex: 2)
         alunos[0].marcado = 2;
+
+        // 3. Reorganizamos para o próximo menor real ir para o topo
         refazerHeap(alunos, 0, TAM_VET);
     }
 
