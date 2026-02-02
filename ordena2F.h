95bfd34931da1a399fc03bf35c7942deacc8981e
paulamaria31 <mariapaulamirandagasparini@gmail.com>
2026-02-02 15:13:54 -0300

    Adicionando 2 mais f fitas .h

---
+++ ordena2F.h
@@ -0,0 +1,23 @@
+﻿#ifndef ordena2F_h
+#define ordena2F_h
+
+#include "Aluno.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+#define F 10 
+#define MEM_TAM 10 
+
+typedef struct {
+    Aluno aluno;
+    int marcado; 
+} RegistroHeap;
+
+// Protótipos das funções
+void ordernarArquivo2F(int quantidade, FILE* arq);
+void faseGeracaoBlocos(FILE* arq, int* numBlocos);
+void faseIntercalacao2F(int numBlocos);
+void intercalarBlocos(int fitaMin, int fitaMax, int fitaDestinoIdx);
+void refazerHeap(RegistroHeap heap[], int i, int n);
+
+#endif
